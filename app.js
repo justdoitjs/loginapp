dd248c681c842cec22bd8fd1a6c08ea4a5ffe011
slim <enthusa@live.com>
2016-06-02 16:42:16 +0200

    helloo

--- app.js
+++ app.js
@@ -81,8 +81,21 @@ app.use(function (req, res, next) {
 
 
 // Set Port
-app.set('port', (process.env.PORT || 3000));
+// app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
+// app.listen(app.get('port'), function(){
+	// console.log('Server started on port '+app.get('port'));
+// });
+
+
+// setup ports
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+
+});
+
+// server listens in on port
+app.listen(server_port, server_ip_address, function () {
+	 console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
 });
